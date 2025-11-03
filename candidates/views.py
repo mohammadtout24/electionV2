6ebb40fb396b7295edae0878ca841fa78d4e2474
mohammadtout24 <mohammadtout24@gmail.com>
2025-11-03 11:12:50 +0200

    Update views.py

--- candidates/views.py
+++ candidates/views.py
@@ -222,7 +222,7 @@ def all_users_list(request):
     CustomUser = get_user_model()
     
     
-    all_users = User.objects.select_related('userprofile').order_by('userprofile__name')
+    all_users = CustomUser.objects.filter(is_superuser=False).select_related('userprofile').prefetch_related('candidate').order_by('userprofile__name')
     context = {
         'all_users': all_users, 
         'is_candidate': True,
